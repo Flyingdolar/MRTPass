7e6ae5a778d7b76fa216c58a1027ce14699a415b
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-05-10 03:36:14 +0800

    fix: cross localhost request

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -12,6 +12,12 @@ module Backend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+          origins '*'
+              resource '*',headers: :any, methods: [:get, :post, :put,:patch, :delete, :options]
+    end
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
