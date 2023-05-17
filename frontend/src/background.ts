d80594a5a19591fa317f6611d31a838ef6ed9d84
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-05-17 22:07:36 +0800

    feat: Change Size of the frame

--- frontend/src/background.ts
+++ frontend/src/background.ts
@@ -14,8 +14,8 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 375,
+    height: 812,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
