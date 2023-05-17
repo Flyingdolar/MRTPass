d78e8f52e56c91fa217859d069f32dec16c9365c
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-05-18 00:52:17 +0800

    feat: Add input validation for account page

--- frontend/src/views/account/MyAccount.vue
+++ frontend/src/views/account/MyAccount.vue
@@ -2,16 +2,17 @@
   <n-form
     ref="formRef"
     :model="model"
+    :rules="rules"
     label-placement="left"
     label-width="auto"
     require-mark-placement="right-hanging"
     size="medium"
     :style="{ maxwidth: '800px', align: ' center ' }"
   >
-    <n-form-item label="Email">
+    <n-form-item label="Email" path="email">
       <n-input v-model:value="model.email" placeholder="請輸入 Email" />
     </n-form-item>
-    <n-form-item label="密碼">
+    <n-form-item label="密碼" path="password">
       <n-input v-model:value="model.password" placeholder="請輸入密碼" />
     </n-form-item>
     <n-button attr-type="button" @click="login"> 登入帳號 </n-button>
@@ -40,34 +41,34 @@ const model = reactive({
   password: "",
 });
 
-// const rules: FormRules = {
-//   emailValue: [
-//     {
-//       required: true,
-//       validator(rule: FormItemRule, value: string) {
-//         if (!value) {
-//           return new Error("Email is required");
-//         } else if (!/@/.test(value)) {
-//           return new Error("Email format is incorrect");
-//         }
-//         return true;
-//       },
-//       trigger: ["login", "regist"],
-//     },
-//   ],
-//   passwordValue: [
-//     {
-//       required: true,
-//       validator(rule: FormItemRule, value: string) {
-//         if (!value) {
-//           return new Error("Password is required");
-//         }
-//         return true;
-//       },
-//       trigger: ["input", "blur"],
-//     },
-//   ],
-// };
+const rules: FormRules = {
+  email: [
+    {
+      required: true,
+      validator(rule: FormItemRule, value: string) {
+        if (!value) {
+          return new Error("Email is required");
+        } else if (!/@/.test(value)) {
+          return new Error("Email format is incorrect");
+        }
+        return true;
+      },
+      trigger: ["input", "blur"],
+    },
+  ],
+  password: [
+    {
+      required: true,
+      validator(rule: FormItemRule, value: string) {
+        if (!value) {
+          return new Error("Password is required");
+        }
+        return true;
+      },
+      trigger: ["input", "blur"],
+    },
+  ],
+};
 
 function login() {
   //axios post
