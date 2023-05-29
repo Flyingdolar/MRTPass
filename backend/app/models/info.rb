a948218314a3a6997fa33d0343f00640e7745b1e
yoyoNTNU <114905338+yoyoNTNU@users.noreply.github.com>
2023-05-29 18:22:29 +0800

    fix:typo

--- backend/app/models/info.rb
+++ backend/app/models/info.rb
@@ -1,5 +1,5 @@
 class Info < ApplicationRecord
-    validates :nme, :photo, :address, :type_, :Des, presence: true
+    validates :name, :photo, :address, :type_, :Des, presence: true
     has_many :station_infos
     has_many :comments
     mount_uploader :photo , InfoUploader
