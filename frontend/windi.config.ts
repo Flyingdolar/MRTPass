7beba6f74daeb8cb85d608b4d4b711298c78218a
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-06-02 00:20:13 +0800

    feat[windi.config.ts]: Add text color

--- frontend/windi.config.ts
+++ frontend/windi.config.ts
@@ -14,6 +14,16 @@ export default defineConfig({
       body: "#444547", // Gray-700
       secondary: "#757A80", // Gray-500
       disabled: "#BABDBF", // Gray-300
+      // Theme Color
+      red: "#BB0F17",
+      orange: "#BE9505",
+      yellow: "#E9B200",
+      green: "#62AE01",
+      teal: "#008E8E",
+      blue: "#0066CC",
+      indigo: "#6B23B4",
+      purple: "#8A3FFC",
+      pink: "#D63384",
     },
     extend: {
       fontFamily: {
