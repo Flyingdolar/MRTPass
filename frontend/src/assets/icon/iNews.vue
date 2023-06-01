b30c19658d5bfba05240dff6f70e69228abd5b76
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-06-01 20:54:45 +0800

    feat[assets]: Setup all icons as vue components

---
+++ frontend/src/assets/icon/iNews.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      x="5"
+      y="5"
+      width="14"
+      height="14"
+      rx="3"
+      stroke="#333333"
+      stroke-width="2"
+    />
+    <path
+      d="M6 10H18"
+      stroke="#333333"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
