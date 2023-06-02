b73034dd598d3c7d0d0bcc632eb5b816e8b811c0
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-06-02 08:46:42 +0800

    feat[App.vue]: Footer UI style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,39 +16,71 @@
     <!-- Footer -->
     <n-layout-footer flex="~" bg="white" pos="relative">
       <!-- Sec01: 捷運資訊 -->
-      <router-link to="/" class="flex-grow" bg="white hover:blue-100">
-        <n-space :vertical="true" align="center" p="x-auto y-2" :size="[0, 0]">
+      <router-link to="/" @click="setBtn(1)" flex="grow">
+        <n-space
+          align="center"
+          class="px-auto py-2 hover:bg-blue-100"
+          :vertical="true"
+          :size="[0, 0]"
+          :bg="isActive(1) ? 'blue-50' : 'white'"
+          :text="isActive(1) ? 'blue' : 'body'"
+          :border="isActive(1) ? 't-2 blue opacity-60' : 't-2 transparent'"
+        >
           <n-icon size="24">
             <compass />
           </n-icon>
-          <div text="secondary xs center" w="12">捷運資訊</div>
+          <div text="xs center" w="12">捷運資訊</div>
         </n-space>
       </router-link>
       <!-- Sec02: 捷運周邊 -->
-      <router-link to="/post" class="flex-grow" bg="white hover:blue-100">
-        <n-space :vertical="true" align="center" p="x-auto y-2" :size="[0, 0]">
+      <router-link to="/post" @click="setBtn(2)" flex="grow">
+        <n-space
+          :vertical="true"
+          align="center"
+          class="px-auto py-2 hover:bg-blue-100"
+          :size="[0, 0]"
+          :bg="isActive(2) ? 'blue-50' : 'white'"
+          :text="isActive(2) ? 'blue' : 'body'"
+          :border="isActive(2) ? 't-2 blue opacity-60' : 't-2 transparent'"
+        >
           <n-icon size="24">
             <star />
           </n-icon>
-          <div text="secondary xs center" w="12">捷運周邊</div>
+          <div text="xs center" w="12">捷運周邊</div>
         </n-space>
       </router-link>
       <!-- Sec03: 遺失物 -->
-      <router-link to="/about" class="flex-grow" bg="white hover:blue-100">
-        <n-space :vertical="true" align="center" p="x-auto y-2" :size="[0, 0]">
+      <router-link to="/about" @click="setBtn(3)" flex="grow">
+        <n-space
+          :vertical="true"
+          align="center"
+          class="px-auto py-2 hover:bg-blue-100"
+          :size="[0, 0]"
+          :bg="isActive(3) ? 'blue-50' : 'white'"
+          :text="isActive(3) ? 'blue' : 'body'"
+          :border="isActive(3) ? 't-2 blue opacity-60' : 't-2 transparent'"
+        >
           <n-icon size="24">
             <bag />
           </n-icon>
-          <div text="secondary xs center" w="12">遺失物</div>
+          <div text="xs center" w="12">遺失物</div>
         </n-space>
       </router-link>
       <!-- Sec04: 我的帳號 -->
-      <router-link to="/account" class="flex-grow" bg="white hover:blue-100">
-        <n-space :vertical="true" align="center" p="x-auto y-2" :size="[0, 0]">
+      <router-link to="/account" @click="setBtn(4)" flex="grow">
+        <n-space
+          :vertical="true"
+          align="center"
+          class="px-auto py-2 hover:bg-blue-100"
+          :size="[0, 0]"
+          :bg="isActive(4) ? 'blue-50' : 'white'"
+          :text="isActive(4) ? 'blue' : 'body'"
+          :border="isActive(4) ? 't-2 blue opacity-60' : 't-2 transparent'"
+        >
           <n-icon size="24">
             <user />
           </n-icon>
-          <div text="secondary xs center" w="12">我的帳號</div>
+          <div text="xs center" w="12">我的帳號</div>
         </n-space>
       </router-link>
     </n-layout-footer>
@@ -77,12 +109,27 @@ import { watchOnce } from "@vueuse/core";
 import store from "./scripts/vuex";
 
 const isLogin = computed(() => (store?.state?.userinfo ? true : false));
+const activeBtn = ref(1);
+
 onMounted(() => {
   if (!isLogin.value) {
     fetchsession();
   }
-  //console.log(store.state.userinfo);
+  console.log(store.state.userinfo);
 });
+
+function setBtn(num: number) {
+  activeBtn.value = num;
+}
+
+function isActive(num: number) {
+  if (num == activeBtn.value) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function fetchsession() {
   //axios get
   axios
