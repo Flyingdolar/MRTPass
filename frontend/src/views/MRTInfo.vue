bf733758b86d758eae85d0bc2768097f88cb8d9e
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-06-09 03:04:03 +0800

    feat[MRTInfo]: Setup depart station

--- frontend/src/views/MRTInfo.vue
+++ frontend/src/views/MRTInfo.vue
@@ -1,10 +1,19 @@
 <template>
-  <h1>This is info</h1>
+  <!-- Header: 選擇出發站點 -->
+  <div p="x-4 y-3" space="y-1" bg="blue-100">
+    <div text="title">選擇出發站點</div>
+    <div flex="~" space="x-4">
+      <n-select :value="depRoute" :options="routeOpt"></n-select>
+      <n-select :value="depRoute" :options="routeOpt"></n-select>
+    </div>
+  </div>
+  <!-- Body: -->
 </template>
 
 <script setup lang="ts">
 import axios from "axios";
 import {
+  NSelect,
   NForm,
   NInput,
   NFormItem,
@@ -16,24 +25,17 @@ import {
 } from "naive-ui";
 import { ref, reactive, computed } from "vue";
 
-const model = reactive({
-  topic: "",
-  context: "",
-});
+let depRoute = ref("");
+let routeOpt = reactive([
+  // FIXME: 路線資料應該改為後端抓取
+  { label: "淡水信義線", value: "R" },
+  { label: "板南線", value: "BL" },
+  { label: "中和新蘆線", value: "O" },
+  { label: "文湖線", value: "BR" },
+  { label: "松山新店線", value: "G" },
+  { label: "小碧潭支線", value: "LG" },
+  { label: "環狀線", value: "Y" },
+]);
 
-function submitpost() {
-  //axios post
-  axios
-    .post("http://localhost:3000/api/mrt_admin/announcement", {
-      topic: model.topic,
-      context: model.context,
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  //axios
-}
+// Get Route Optiona
 </script>
