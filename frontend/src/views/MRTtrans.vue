e2071168f437f1dc1085ec4d00e01f028ac241da
Larry Yang <128600823+Larry1220@users.noreply.github.com>
2023-06-13 02:59:55 +0800

    feat: timetableedit API

--- frontend/src/views/MRTtrans.vue
+++ frontend/src/views/MRTtrans.vue
@@ -2,6 +2,88 @@
   <n-space size="large" line-height="20px" vertical>
     <n-card>
       <n-h3 class="cardtitle">trans</n-h3>
+      <n-card
+        v-for="item in CurrentTrans"
+        :key="item?.id"
+        footer-style="padding: 0;"
+        :bordered="false"
+      >
+        <template #header>
+          <div justify="items-end" flex="~" w="full">
+            <div flex="grow" text="lg title">
+              {{ item.name }}
+            </div>
+          </div>
+        </template>
+        {{ item.Des }}
+        <template #footer>
+          <div
+            flex="~"
+            justify="center items-center"
+            p="x-4 y-2"
+            v-if="roleisAdmin"
+          >
+            <n-button
+              @click="showOldTrans(item.id)"
+              flex="~ grow"
+              size="medium"
+              type="info"
+              quaternary
+            >
+              <template #icon>
+                <n-icon :size="18"><edit /></n-icon>
+              </template>
+              <div>編輯</div>
+            </n-button>
+            <div m="1" p="0.8px" bg="gray-200" />
+            <n-button
+              @click="showConfirmDelete(item.id)"
+              flex="~ grow"
+              size="medium"
+              type="error"
+              quaternary
+            >
+              <template #icon>
+                <n-icon :size="18"><trash /></n-icon>
+              </template>
+              <div>刪除</div>
+            </n-button>
+          </div>
+        </template>
+      </n-card>
+
+      <!-- Overlay: Confirm Delete -->
+      <n-modal v-model:show="ConfirmDeleteshow">
+        <n-card
+          w="3/4 min-30"
+          title="確定要刪除？"
+          :header-style="{ 'align-self': 'center' }"
+        >
+          <div text="sm center secondary" pb="2">請注意，刪除後無法復原</div>
+          <template #footer>
+            <div flex="~ gap-8" class="justify-center px-2">
+              <n-button @click="deleteTrans" type="error" flex="grow" ghost>
+                <template #icon>
+                  <n-icon :size="18"><trash /></n-icon>
+                </template>
+                <div>刪除</div>
+              </n-button>
+              <n-button
+                @click="ConfirmDeleteshow = false"
+                type="tertiary"
+                flex="grow"
+                ghost
+              >
+                <template #icon>
+                  <n-icon :size="18"><back /></n-icon>
+                </template>
+                <div>取消</div>
+              </n-button>
+            </div>
+          </template>
+        </n-card>
+      </n-modal>
+      <!-- Overlay: New Annoucement -->
       <n-space v-if="roleisAdmin" justify="center">
         <n-button @click="showNewTrans = true">新增轉乘資訊</n-button>
       </n-space>
@@ -114,7 +196,13 @@ import axios from "axios";
 import router from "@/router";
 import store from "@/scripts/vuex";
 import { useRoute } from "vue-router";
-import { Station, Line, LineStation, AlotsLineStation } from "../scripts/types";
+import {
+  Station,
+  Line,
+  LineStation,
+  AlotsLineStation,
+  Trans,
+} from "../scripts/types";
 import back from "../assets/icon/iRefund.vue";
 import add from "../assets/icon/iAdd.vue";
 import edit from "../assets/icon/iEdit.vue";
@@ -181,6 +269,7 @@ const rules: FormRules = {
 watch(store.state, () => {
   getTrans();
 });
+const CurrentTrans = ref<Trans[]>();
 function getTrans() {
   if (store.state.currentlinestation) {
     //axios get
@@ -192,6 +281,8 @@ function getTrans() {
       })
       .then(function (response) {
         console.log(response);
+        CurrentTrans.value = response.data.data;
+        console.log(CurrentTrans.value);
       })
       .catch(function (error) {
         console.log(error);
@@ -310,6 +401,7 @@ onBeforeMount(() => {
       console.log(error);
     });
   //axios
+  getTrans();
 });
 function NewTransSubmitt() {
   const goodformcheck = ref(true);
@@ -318,12 +410,44 @@ function NewTransSubmitt() {
       goodformcheck.value = false;
     }
   });
+  const itemaddress = ref("");
+  CurrentLineStation.value[0].item.forEach(function (item, index, array) {
+    if (item.station.id == Current.value.dynamicInputValue[0].CurrentStation) {
+      itemaddress.value = item.station.name;
+    }
+  });
+  const stationid1 = computed(() => {
+    if (Current.value.dynamicInputValue[0]) {
+      return Current.value.dynamicInputValue[0].CurrentStation;
+    } else {
+      return null;
+    }
+  });
+  const stationid2 = computed(() => {
+    if (Current.value.dynamicInputValue[1]) {
+      return Current.value.dynamicInputValue[1].CurrentStation;
+    } else {
+      return null;
+    }
+  });
+  const stationid3 = computed(() => {
+    if (Current.value.dynamicInputValue[2]) {
+      return Current.value.dynamicInputValue[2].CurrentStation;
+    } else {
+      return null;
+    }
+  });
   if (goodformcheck.value) {
     //axios post
     axios
       .post("http://localhost:3000/api/mrt_admin/trans", {
         name: model.name,
+        photo: formData,
+        address: itemaddress.value,
         Des: model.context,
+        station_id_1: stationid1.value,
+        station_id_2: stationid2.value,
+        station_id_3: stationid3.value,
       })
       .then(function (response) {
         console.log(response.data.data);
@@ -335,5 +459,47 @@ function NewTransSubmitt() {
   } else {
     message.error("請填滿空欄或刪除多餘的");
   }
+}
+const formData = new FormData();
+function handleImgBefore(event: any) {
+  const file = event.file;
+  const isImage = file.type.includes("image");
+  const fileSize = file.file.size / 1024 / 1024;
+  // TODO: 顯示錯誤訊息
+  if (!isImage) console.error("上傳內容必須為圖片格式!");
+  if (fileSize > 2) console.error("上傳圖片大小不能超過2MB!");
+  return isImage && fileSize < 2;
+}
+
+function handleImgChange(event: any) {
+  formData.append("picture", event[0].file, event[0].file.name);
+}
+
+function handleImgRemove() {
+  formData.delete("picture");
+}
+
+function cancelChange() {
+  formData.delete("picture");
+}
+const TransID = ref(-1);
+const ConfirmDeleteshow = ref(false);
+const OldTransshow = ref(false);
+const OldTransindex = ref(-1);
+function showConfirmDelete(id: number) {
+  ConfirmDeleteshow.value = true;
+  TransID.value = id;
+  //console.log(AnnouncementID);
+}
+function showOldTrans(id: number) {
+  OldTransshow.value = true;
+  TransID.value = id;
+  //console.log(AnnouncementID);
+  CurrentTrans.value.forEach((item, index) => {
+    if (item.id == TransID.value) {
+      OldTransindex.value = index;
+    }
+  });
+  //console.log(OldAnnounceindex.value);
 }
 </script>
