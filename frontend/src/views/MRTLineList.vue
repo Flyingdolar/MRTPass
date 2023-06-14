98fae27f85960dd4887ce44477f6dc6c9025eddf
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-06-14 14:36:31 +0800

    feat[MRTLineList]: Setup page UI

--- frontend/src/views/MRTLineList.vue
+++ frontend/src/views/MRTLineList.vue
@@ -1,38 +1,64 @@
 <template>
-  <n-card class="card">
-    <n-space justify="space-around" size="large" line-height="20px">
-      <n-button @click="goback" size="large">返回</n-button>
-      <n-h3 class="cardtitle">捷運路線</n-h3>
-      <n-button @click="tmp" size="large">搜尋</n-button>
-    </n-space>
-    <n-space justify="center" class="content2">
-      <n-card
-        :header-style="{ 'align-self': 'center' }"
-        :footer-style="{ 'align-self': 'center' }"
-        justify="center"
+  <div flex="~ col" justify="center items-end" h="10" bg="white">
+    <div flex="~" m="t-auto b-10px x-6" justify="items-center">
+      <n-button @click="goback" quaternary>
+        <n-icon :size="20"><back /></n-icon>
+      </n-button>
+      <div
+        flex="grow"
+        mt="1"
+        text="title center bottom"
+        style="font-size: 16px"
       >
-        <div v-for="item in colData" :key="item?.id">
-          <n-card>
-            <n-space align="start" size="large">
-              <n-h3>{{ item.name }}</n-h3>
-              <n-space align="end" size="large">
-                <n-button @click="EditLine(item.id)">編輯</n-button
-                ><n-popconfirm @positive-click="DeleteLine(item.id)"
-                  ><template #trigger><n-button>刪除</n-button></template
-                  >確認刪除</n-popconfirm
-                >
-              </n-space>
-            </n-space>
-          </n-card>
+        捷運路線
+      </div>
+      <n-button @click="tmp" quaternary>
+        <n-icon :size="20"><search /></n-icon>
+      </n-button>
+    </div>
+    <div p="y-1pt" bg=" gray-200" />
+  </div>
+  <div flex="~ col gap-3" h="160" overflow="auto">
+    <div py="3px" />
+    <div v-for="line in colData" :key="line.id" bg="white">
+      <div flex="~" m="x-6 y-5" justify="items-center">
+        <div
+          flex="~ grow gap-4"
+          m="y-auto l-4"
+          text="body"
+          justify="items-center"
+        >
+          <div
+            w="8"
+            border="2 rounded-lg opacity-20"
+            :style="setLineColor(line.colorcode)"
+          >
+            <div text="center">{{ line.linecolor }}</div>
+          </div>
+          <div>{{ line.name }}</div>
         </div>
-      </n-card>
-    </n-space>
-  </n-card>
+        <div flex="~ gap-6">
+          <n-button ghost @click="DeleteLine(line.id)">
+            <template #icon>
+              <n-icon text="danger"><remove /></n-icon>
+            </template>
+          </n-button>
+          <n-button quaternary @click="EditLine(line.id)">
+            <template #icon>
+              <n-icon><goto /></n-icon>
+            </template>
+          </n-button>
+        </div>
+      </div>
+    </div>
+    <div py="3px" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import {
   NCard,
+  NIcon,
   NSpace,
   NButton,
   NModal,
@@ -41,6 +67,10 @@ import {
   NH3,
   NPopconfirm,
 } from "naive-ui";
+import back from "../assets/icon/iExpLeft.vue";
+import goto from "../assets/icon/iExpRight.vue";
+import remove from "../assets/icon/iTrash.vue";
+import search from "../assets/icon/iSearch.vue";
 import type { DataTableColumns } from "naive-ui";
 import { computed, h, onMounted, ref, watch } from "vue";
 import { watchOnce } from "@vueuse/core";
@@ -55,11 +85,13 @@ onMounted(() => {
   axios
     .get("http://localhost:3000/api/mrt_admin/line")
     .then(function (response) {
-      //console.log(response);
+      console.log(response);
       colData.value = response.data.data.map(function (item, index, array) {
         return {
           id: item.id,
           name: item.name,
+          linecolor: item.linecolor,
+          colorcode: item.colorcode,
         };
       });
       //console.log(colData.value);
@@ -69,6 +101,10 @@ onMounted(() => {
     });
   //axios
 });
+
+function setLineColor(color: string) {
+  return "color: " + color + "; border-color: " + color + ";";
+}
 
 function EditLine(id: number) {
   router.push("/linelist/" + (id as unknown as string));
@@ -92,3 +128,9 @@ function goback() {
   router.push("/profile");
 }
 </script>
+
+<style scoped>
+::-webkit-scrollbar {
+  display: none;
+}
+</style>
